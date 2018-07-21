8c634e2616f4b2b39ad30fa3d60d2facd8743628
Joris De Smedt <joris.desmedt@lightspeedpos.com>
2018-07-22 01:38:56 +0200

    Added nestjs + changed directory structure

---
+++ jest.client.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  roots: ['client/src'],
+  setupTestFrameworkScriptFile: '<rootDir>/client/src/setup-jest.ts',
+  moduleNameMapper: {
+    '@app/(.*)': '<rootDir>/client/src/app/$1',
+    '@assets/(.*)': '<rootDir>/client/src/assets/$1',
+    '@core/(.*)': '<rootDir>/client/src/app/core/$1',
+    '@env': '<rootDir>/client/src/environments/environment',
+    '@src/(.*)': '<rootDir>/client/src/$1',
+    '@state/(.*)': '<rootDir>/client/src/app/state/$1'
+  },
+  globals: {
+    'ts-jest': {
+      tsConfigFile: 'client/src/tsconfig.spec.json'
+    },
+    __TRANSFORM_HTML__: true
+  }
+};
